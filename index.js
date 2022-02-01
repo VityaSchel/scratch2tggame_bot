3a73ef37575eeb2692a1a8e6fffc87d738c74f48
Viktor <vityaschel@utidteam.com>
2022-02-01 12:37:17 +0400

    test

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ bot.on('inline_query', async inlineQuery => {
 })
 
 bot.on('callback_query', async callbackQuery => {
-  console.log(callbackQuery.message.reply_markup)
+  console.log(callbackQuery.message.reply_markup.inline_keyboard[0][0])
   if(callbackQuery.game_short_name === 'custom') {
     const projectID = await db.get(callbackQuery.inline_message_id)
     bot.answerCallbackQuery(callbackQuery.id, { url: `https://scratch2tggame.utidteam.com/${projectID}` })
