a57dd8f1b475d459451ce32be73e3c8c3e550c5b
Viktor <vityaschel@utidteam.com>
2022-02-01 12:29:09 +0400

    fix??? hopefully

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ bot.on('inline_query', async inlineQuery => {
 
 bot.on('callback_query', async callbackQuery => {
   if(!callbackQuery.game_short_name) {
-    const projectID = callbackQuery.data.replaceAll(/^[^\d]+$/g, '')
+    const projectID = callbackQuery.data.replaceAll(/[^\d]/g, '')
     bot.answerCallbackQuery(callbackQuery.id, { url: `https://scratch2tggame.utidteam.com/${projectID}` })
   } else {
     bot.answerCallbackQuery(callbackQuery.id, { url: `https://scratch2tggame.utidteam.com/${callbackQuery.game_short_name}` })
