dce681f60547aa9bb8f9d2189c982e97cfccf70a
Viktor <vityaschel@utidteam.com>
2022-02-01 12:38:53 +0400

    trying

--- index.js
+++ index.js
@@ -55,9 +55,8 @@ bot.on('inline_query', async inlineQuery => {
 })
 
 bot.on('callback_query', async callbackQuery => {
-  console.log(callbackQuery.message.reply_markup.inline_keyboard[0][0])
   if(callbackQuery.game_short_name === 'custom') {
-    const projectID = await db.get(callbackQuery.inline_message_id)
+    const projectID = await db.get(callbackQuery.message.id)
     bot.answerCallbackQuery(callbackQuery.id, { url: `https://scratch2tggame.utidteam.com/${projectID}` })
   } else {
     bot.answerCallbackQuery(callbackQuery.id, { url: `https://scratch2tggame.utidteam.com/${callbackQuery.game_short_name}` })
